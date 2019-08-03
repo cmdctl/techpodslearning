30625bb58b5a273eb3f0d38b12f71e003d16abb4
Anton Bozhinov <anton.bozhinov@ovoenergy.com>
2019-08-03 16:34:15 +0300

    init

---
+++ email/email.go
@@ -0,0 +1,84 @@
+package email
+
+import (
+	"bytes"
+	"fmt"
+	"mime/quotedprintable"
+	"net/smtp"
+	"strings"
+)
+
+const (
+	SMTPServer     = "smtp.gmail.com"
+	SMTPServerPort = "587"
+)
+
+type Sender struct {
+	User     string
+	Password string
+}
+
+func NewSender(Username, Password string) Sender {
+	return Sender{Username, Password}
+}
+
+func (sender Sender) SendMail(Dest []string, Subject, bodyMessage string) {
+	msg := "From: " + sender.User + "\n" +
+		"To: " + strings.Join(Dest, ",") + "\n" +
+		"Subject: " + Subject + "\n" + bodyMessage
+
+	err := smtp.SendMail(
+		SMTPServer+":"+SMTPServerPort,
+		smtp.PlainAuth("", sender.User, sender.Password, SMTPServer),
+		sender.User, Dest, []byte(msg),
+	)
+
+	if err != nil {
+		fmt.Printf("smtp error: %s", err)
+		return
+	}
+
+	fmt.Println("Mail sent successfully!")
+}
+
+func (sender Sender) WriteEmail(dest []string, contentType, subject, bodyMessage string) string {
+	header := make(map[string]string)
+	header["From"] = sender.User
+
+	recipient := ""
+
+	for _, user := range dest {
+		recipient = recipient + user
+	}
+
+	header["To"] = recipient
+	header["Subject"] = subject
+	header["MIME-Version"] = "1.0"
+	header["Content-Type"] = fmt.Sprintf("%s; charset=\"utf-8\"", contentType)
+	header["Content-Transfer-Encoding"] = "quoted-printable"
+	header["Content-Disposition"] = "inline"
+
+	message := ""
+
+	for key, value := range header {
+		message += fmt.Sprintf("%s: %s\r\n", key, value)
+	}
+
+	var encodedMessage bytes.Buffer
+
+	finalMessage := quotedprintable.NewWriter(&encodedMessage)
+	_, _ = finalMessage.Write([]byte(bodyMessage))
+	_ = finalMessage.Close()
+
+	message += "\r\n" + encodedMessage.String()
+
+	return message
+}
+
+func (sender *Sender) WriteHTMLEmail(dest []string, subject, bodyMessage string) string {
+	return sender.WriteEmail(dest, "text/html", subject, bodyMessage)
+}
+
+func (sender *Sender) WritePlainEmail(dest []string, subject, bodyMessage string) string {
+	return sender.WriteEmail(dest, "text/plain", subject, bodyMessage)
+}
---
+++ server/app.go
@@ -0,0 +1,23 @@
+package server
+
+import (
+	"github.com/AntonBozhinov/techpodslearn/email"
+	"github.com/gin-gonic/gin"
+	"gopkg.in/mgo.v2"
+	"net/http"
+)
+
+type Server struct {
+	Router      gin.Engine
+	Mongo       *mgo.Database
+	EmailSender email.Sender
+}
+
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	s.Router.ServeHTTP(w, r)
+}
+
+func NewServer(router gin.Engine, mongo *mgo.Database, emailSender email.Sender) *Server {
+	return &Server{Router: router, Mongo: mongo, EmailSender: emailSender}
+}
+
